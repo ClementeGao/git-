b4ff3b6fea77c6c69806da00f316d95ab4bd3c98
Clement <809668491@qq.com>
2020-04-26 20:24:04 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git测试
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
