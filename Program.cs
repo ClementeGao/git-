a283eccf14b2e18ffa3c2bc4d0b1d8bc6d2b9228
Clement <809668491@qq.com>
2020-04-26 20:26:37 +0800

    add code 1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace git测试
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("first commit code");
         }
     }
 }
