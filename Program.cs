de73642a4f5c586b70cb50c711d8ec844057b952
Clement <809668491@qq.com>
2020-04-26 21:00:42 +0800

    new amend

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace git测试
             Console.WriteLine("Hello World!");
             Console.WriteLine("sencod add code");
             Console.WriteLine("forth add code");
+            Console.WriteLine("测试 acmend命令");
         }
     }
 }
